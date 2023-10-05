fc9f45da37bc88c86b851d747e1b0ecc3b40c20e
David Montemayor <montemayor1990@gmail.com>
2023-10-05 13:49:38 -0500

    external_js_exercise

---
+++ js/external.js
@@ -0,0 +1,23 @@
+//console.log("Hello from external JavaScript")
+// Use the alert function to show a message that says 'Welcome to my Website!'.
+//alert("Welcome to my website");
+//
+//     Use a prompt to ask for the user's favorite color. Use the user's response to alert a message that says that the color entered is your favorite color too.
+// let userInput = prompt('Enter favorite color');
+// console.log(`The user entered: ${userInput}`);
+// alert(`${userInput} is my favorite color too`);
+//
+// let hrsFb = 10;
+// let hrsG = 6;
+// let hrsAmz = 4;
+let fbPay = 350;
+let googlePay = 400;
+let amzPay = 380;
+let google = prompt("How many hours did you work at google?");
+alert(`${google * googlePay}`);
+let facebook = prompt("How many hours did you work at facebook?");
+alert(`${facebook * fbPay}`);
+let amazon = prompt("How many hours did you work at amazon?");
+alert(`${amazon * amzPay}`);
+
+console.log(facebook*hrsFb + google*hrsG + amazon*hrsAmz);
