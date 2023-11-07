06d673325c0d86ad53493c2099474fa2720e2bc4
David Montemayor <montemayor1990@gmail.com>
2023-11-07 08:59:38 -0600

    dom

---
+++ js/dom-query-js.js
@@ -0,0 +1,19 @@
+"use strict";
+
+
+// TODO: Uncomment next line and get the main header element by id
+const mainHeader = document.querySelector(".main-header");
+
+
+// TODO: Uncomment the next line and get the sub header element by id
+const subHeader = document.querySelector(".sub-header");
+
+
+// TODO: Uncomment the next line and get all list items
+ const listItems = document.querySelectorAll("ul li");
+
+
+// TODO: Uncomment the next line and get all elements with class name sub-paragraph
+ const subParagraphs = document.querySelectorAll(".sub-paragraph");
+
+
